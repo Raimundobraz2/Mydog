0482a4bab01d98ff0c33fdbf8b1f5d953173a7ad
Raimundo braz <wilsonquilenguebraz@gmail.com>
2023-11-20 18:58:20 +0100

    Projeto terminado

---
+++ script.js
@@ -0,0 +1,16 @@
+const sr  = ScrollReveal({
+    distance: '35px',
+    duration:  2400,
+    reset:  true
+});
+
+sr.reveal( '.logotipo',{delay:210,  origin: 'left'});
+sr.reveal( '.navlist',{delay:310,  origin: 'top'});
+sr.reveal( '.icone-nav',{delay:310,  origin: 'top'});
+
+sr.reveal( '.conteudo',{delay:410,  origin: 'right'});
+sr.reveal( '.conteudo-button',{delay:1010,  origin: 'botton'});
+sr.reveal( '.img-mydog',{delay:610,  origin: 'right'});
+sr.reveal( 'p',{delay:510,  origin: 'right'});
+sr.reveal( '.conteudo-img' ,{delay:710,  origin: 'left'});
+sr.reveal( '.icone-social' ,{delay:810,  origin: 'left'});
